c447f348ca99ed365762f78a9063dc18c0664698
unknown <saeedbutt320@.com>
2019-04-20 04:39:34 +0200

    Added  more tests

--- Test/apptest.js
+++ Test/apptest.js
@@ -6,6 +6,20 @@ describe('App', function()
 {
     it('should return Hello Wolrd', function()
     {
-        asset.equal(app(),'Hello World');
+        let output = app.helloWorld();
+        asset.equal(output,'Hello World');
+    })
+    it('Output should be type of string',function(){
+        let output = app.helloWorld();
+        asset.typeOf(output,'string');
+    })
+    it('It should reutrn multiply of two numbers', function()
+    {
+        let output= app.multiplyNumbers(5,2);
+        asset.equal(output,10);
+    })
+    it('Output should be greater than 10',function(){
+        let output = app.multiplyNumbers(6,2);
+        asset.isAbove(output,10);
     })
 })
